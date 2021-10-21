8f0046d2c4dd7878ac55749e839489435ca4c75b
igor-chalenko <prostakov80@mail.ru>
2021-10-21 17:14:34 +0300

    doxygen-cmake-17 `Package found` message is incorrect

--- cmake/find-doxygen-cmake.cmake
+++ cmake/find-doxygen-cmake.cmake
@@ -7,12 +7,12 @@ if (TARGET Doxygen::doxygen)
     # to find the path to this module rather than the path to a calling list file
     get_filename_component(_current_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
     set_property(GLOBAL PROPERTY _current_dir "${_current_dir}")
+    set(DOXYGEN_CMAKE_VERSION 0.1)
 
     find_package_handle_standard_args(
             doxygen-cmake
-            REQUIRED_VARS DOXYGEN_EXECUTABLE
-            VERSION_VAR DOXYGEN_VERSION
-            HANDLE_COMPONENTS
+            REQUIRED_VARS PACKAGE_PREFIX_DIR
+            VERSION_VAR DOXYGEN_CMAKE_VERSION
     )
 
     # "New" IN_LIST syntax
@@ -20,5 +20,5 @@ if (TARGET Doxygen::doxygen)
 
     include(${_current_dir}/add-docs.cmake)
 else()
-    message(STATUS "Doxygen is not installed, DoxygenCMake is disabled.")
+    message(STATUS "Doxygen is not installed, doxygen-cmake is disabled.")
 endif()
