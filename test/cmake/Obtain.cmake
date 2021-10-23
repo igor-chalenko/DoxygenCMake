991c8fa007adffe2fd46157509a58d242b94e426
igor-chalenko <prostakov80@mail.ru>
2021-10-23 08:41:39 +0300

    doxygen-cmake-21 Fix config API.

--- test/cmake/Obtain.cmake
+++ test/cmake/Obtain.cmake
@@ -1,18 +1,12 @@
-get_filename_component(_current_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
-set_property(GLOBAL PROPERTY current.dir "${_current_dir}")
-
 macro(obtain _name)
     find_package(${_name} QUIET ${ARGN})
 endmacro()
 
 macro(add_to_registry _module _path)
-    get_property(_current_dir GLOBAL PROPERTY current.dir)
     set_property(GLOBAL PROPERTY module.path.${_module} "${_path}")
 endmacro()
 
 macro(import _module)
-    get_property(_current_dir GLOBAL PROPERTY current.dir)
-
     if (${_module} MATCHES "(.+)::(.+)")
         get_property(_path GLOBAL PROPERTY module.path.${CMAKE_MATCH_1})
         if (NOT _path)
